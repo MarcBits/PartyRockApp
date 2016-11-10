d73fbd713e72343211e78c48fb0689fc8c96ab16
Marc Cruz <mcruz@Marcs-MacBook-Air.local>
2016-11-09 21:48:34 -0500

    lecture-62
    Laid out main screen and detail view.

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Marc Cruz on 11/9/16.
+//  Copyright © 2016 MarcBits. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
